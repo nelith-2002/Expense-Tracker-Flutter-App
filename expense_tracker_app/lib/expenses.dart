80a7c2fe291f84aa8058d10c3329bdc5d8a2befd
Nelith Nethsanda <139336822+nelith-2002@users.noreply.github.com>
2026-02-21 00:25:12 +0530

    feat: create basic expenses & expenses list setup

---
+++ expense_tracker_app/lib/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:expense_tracker_app/expenses_list.dart';
+import 'package:expense_tracker_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 18,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 18,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+        ],
+      ),
+    );
+  }
+}
